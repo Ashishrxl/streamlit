b0854807969691cc4a327ce2bef889624402c092
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2025-10-08 10:09:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,9 +58,9 @@ st.link_button("Go to CSVVISUALISATION", "https://csvvisualisation.streamlit.app
 
 st.link_button("Go to AUDIOSTORY", "https://ashishraxaul.streamlit.app/audiostory")
 
-st.link_button("Go to AUDIOSTORY2", "https://ashishraxaul.streamlit.app/audiostory2")
+# st.link_button("Go to AUDIOSTORY2", "https://ashishraxaul.streamlit.app/audiostory2")
 
-st.link_button("Go to AUDIOSTORY3", "https://ashishraxaul.streamlit.app/audiostory3")
+# st.link_button("Go to AUDIOSTORY3", "https://ashishraxaul.streamlit.app/audiostory3")
 
 
 st.link_button("Go to SINGIFY", "https://ashishraxaul.streamlit.app/singify")
